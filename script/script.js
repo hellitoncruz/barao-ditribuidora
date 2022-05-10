0fca17090195613302b2a1107c4389bc1fe6fb69
Helliton <hellitoncruz16@gmail.com>
2022-05-10 20:14:48 -0300

    feature: add images

---
+++ script/script.js
@@ -0,0 +1,13 @@
+// Buscar produtos ao digitar o nome no unput de busca, filtrando aqueles que tiverem os caracteres digitados.
+
+// Número de produtos no carrinho ser representado por um <span>
+        // -> Aumentar e diminuir o valor do numero de acordo o length de produtos.
+
+// Criar elemento (CreateElement) toda vez que o botão "adicionar ao carrinho" for clicado, criando uma <div> com o nome do produto.
+
+// Side bar que aparece para dar uma olhada no carrinho e saber qual valor está até o momento
+
+// Página principal para cada produto
+
+// Cálculo do frete:
+        // -> API de cidades e distância para a cidade que fornece os produtos. E o valor do frete usando o preço fixo vezes a distancia.
